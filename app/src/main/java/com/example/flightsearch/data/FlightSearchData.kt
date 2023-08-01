3aed0753f5d92ef47298a3dafebd283b532b7599
Mirror83 <glenochieng045@gmail.com>
2023-08-01 21:06:53 +0300

    Add FlightSearchData singleton

---
+++ app/src/main/java/com/example/flightsearch/data/FlightSearchData.kt
@@ -0,0 +1,10 @@
+package com.example.flightsearch.data
+
+object FlightSearchData {
+    val airportList: List<Airport> = listOf(
+        Airport(1, "FCO", "Leonardo Da Vinci International Airport", 32781),
+        Airport(2, "JKA", "Jomo Kenyatta International Airport", 32781),
+        Airport(4, "VIE", "Vienna International Airport", 32781),
+        Airport(5, "ATH", "Athens International Airport", 32781),
+    )
+}

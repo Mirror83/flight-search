b6b3c813c8d7b8f71fd256342a2943937b6f27fa
Mirror83 <glenochieng045@gmail.com>
2023-08-04 20:44:43 +0300

    Mark Airport as a Room entity

--- app/src/main/java/com/example/flightsearch/data/Airport.kt
+++ app/src/main/java/com/example/flightsearch/data/Airport.kt
@@ -1,8 +1,16 @@
 package com.example.flightsearch.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "airport")
 data class Airport(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "iata_code", index = true)
     val iataCode: String,
+    @ColumnInfo(index = true)
     val name: String,
     val passengers: Int
 )

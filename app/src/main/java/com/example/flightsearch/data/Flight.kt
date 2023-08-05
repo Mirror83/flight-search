b4b44183b9d9e00901335e6c289ab523a00d2e80
Mirror83 <glenochieng045@gmail.com>
2023-08-05 12:13:02 +0300

    Replaced isFavorite with a nullable favoriteId

--- app/src/main/java/com/example/flightsearch/data/Flight.kt
+++ app/src/main/java/com/example/flightsearch/data/Flight.kt
@@ -3,5 +3,5 @@ package com.example.flightsearch.data
 data class Flight(
     val departureAirport: Airport,
     val arrivalAirport: Airport,
-    val isFavourite: Boolean = false
+    val favoriteId: Int? = null
 )
